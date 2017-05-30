2f87ff98685f76a531e6a46d5205ddc153961b0d
pleuren <div@livemail.tw>
2017-05-30 22:28:34 +0800

    fix bugs

--- chname.py
+++ chname.py
@@ -5,15 +5,14 @@ i = 0
 for f in fileList:
     subPath = os.path.join(os.getcwd(), f)
     if os.path.isdir(subPath) and subPath != os.path.join(os.getcwd(), 'train') and subPath != os.path.join(os.getcwd(), 'val'):
-        i = i + 1
         subList = os.listdir(subPath)
         train = int(round(len(subList)*0.8))
         j = 0
         for f2 in subList:
-            j = j + 1
             print '{}:{:02d}{:03d}'.format(f2, i, j)
             if j < train:
                 copyfile(os.path.join(os.getcwd(), f, f2), os.path.join(os.getcwd(), 'train', '{:02d}{:03d}.jpg'.format(i,j)))
             else:
                 copyfile(os.path.join(os.getcwd(), f, f2), os.path.join(os.getcwd(), 'val', '{:02d}{:03d}.jpg'.format(i,j)))
-            
+            j = j + 1
+        i = i + 1
